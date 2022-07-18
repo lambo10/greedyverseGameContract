540b0e66e196cafb148b6c502a66f7bd7794a2f0
"Lambert Nnadi" <"nnadi406@gmail.com">
2022-07-18 14:37:10 +0100

    new commit

--- greedyverse/game.sol
+++ greedyverse/game.sol
@@ -10,7 +10,7 @@ contract greedyverseGame is ERC1155, Ownable{
     
     uint256[30] public healthCost = [250,700,0,0,200,380,0,0,200,380,450,1150,2020,800,1493,55,30,1000,25,50,2600,1900,1500,370,50,150,30,10,100,0];
      
-    address public tokenContract_add = 0x8B852C7a7d54A5F63742Ca3a1DAFED5D7d74e2A2;
+    address public tokenContract_add = 0xb546fC62DcB523C4f5F1581021Bf27A8019b5516;
     IERC20 public token = IERC20(tokenContract_add);
 
     constructor() ERC1155(""){
